2a8fc7f5b80c0de10b0ad633e45223b1a05b7977
Sasha Raimov <crazyysasha@gmail.com>
2022-01-31 17:10:17 +0500

    adding yandex maps component

--- src/main.js
+++ src/main.js
@@ -8,6 +8,6 @@ import '@/assets/styles/tailwind.css'
 
 
 createApp(App)
-    .use(store)    
+    .use(store)
     .use(router)
     .mount('#app')
--- src/views/EventsView.vue
+++ src/views/EventsView.vue
@@ -1,12 +1,34 @@
 <template>
-    <div>
-        Карта
-    </div>
+    <yandex-map
+        :settings="{
+            apiKey: '05604d63-2945-4cef-bd92-e82c5362bf26',
+            lang: 'ru_RU',
+            enterprise: false,
+            version: '2.1',
+        }"
+        :controls="[]"
+        :coords="[54.82896654088406, 39.831893822753904]"
+        class="w-full"
+    >   
+    <div></div>
+        <!-- <ymap-marker
+            :coords="[54.82896654088406, 39.831893822753904]"
+            marker-id="123"
+        /> -->
+    </yandex-map>
 </template>
 
 
 <script>
-export default {};
+import { 
+    // ymapMarker,
+     yandexMap } from "vue-yandex-maps";
+export default {
+    components: {
+        yandexMap,
+        // ymapMarker,
+    },
+};
 </script>
 
 <style>
