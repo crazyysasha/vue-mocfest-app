b280b9d94df4e8f1088b0a4bea271c1db34f5bb3
aleksejem <alexeyhong10@gmail.com>
2022-05-20 17:35:05 +0500

    Hide tnx text #2.

--- src/views/OrderStatusView.vue
+++ src/views/OrderStatusView.vue
@@ -21,7 +21,7 @@
                 </template>
             </h2>
 
-            <p v-if="order?.status !== 'paid'" class="font-montserrat text-center mb-5 tracking-normal" v-html="$t('orderStatus.text', {email: data.email})">
+            <p v-if="order?.status === 'paid'" class="font-montserrat text-center mb-5 tracking-normal" v-html="$t('orderStatus.text', {email: data.email})">
             </p>
             <button
                 class="
